b110062bb154022a321f0d5e68d79abf3bd2c266
Gabe Olivares <gabeolivares@Gabes-MacBook-Pro.local>
2016-10-18 17:30:13 -0500

    Updated UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/index'
+ root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  # See how all your routes lßay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
